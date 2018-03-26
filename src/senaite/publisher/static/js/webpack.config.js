c591932e3838902df0f62d3ed9e189932c0c251d
Ramon Bartl <rb@ridingbytes.com>
2018-03-26 09:35:40 +0200

    generate JS in js directory

--- src/senaite/publisher/static/js/webpack.config.js
+++ src/senaite/publisher/static/js/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 
   output: {
     filename: 'senaite.publisher.[name].js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './js'),
   },
   module: {
     rules: [
