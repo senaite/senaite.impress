1b8a8266ebae5ed579e84adeeaaf7c49b60a37cf
Ramon Bartl <rb@ridingbytes.com>
2018-09-15 13:04:55 +0200

    Include uncooked JS resources from portal_javascripts (#35)

--- src/senaite/impress/javascripts.py
+++ src/senaite/impress/javascripts.py
@@ -19,7 +19,7 @@ INCLUDE = [
 
 
 class JavaScriptsView(BrowserView):
-    """Helper View to inject JS from portal_javascripts
+    """Helper View to inject uncooked JS from portal_javascripts
 
     Most of it copied from Products.ResourceRegistries
     https://github.com/plone/Products.ResourceRegistries/blob/master/Products/ResourceRegistries/browser/scripts.pt
@@ -34,36 +34,26 @@ class JavaScriptsView(BrowserView):
     def scripts(self):
         registry = self.registry()
         registry_url = registry.absolute_url()
-        context = aq_inner(self.context)
-
-        scripts = registry.getEvaluatedResources(context)
         skinname = url_quote(self.skinname())
+        # get the uncooked resources
+        scripts = registry.getResources()
         result = []
 
         for script in scripts:
-
+            # only consider enabled JS resources
+            if not script.getEnabled():
+                continue
             # Only include JS where the regular expression matches
             script_id = script.getId()
             if not any(map(lambda rx: re.findall(rx, script_id), INCLUDE)):
                 continue
 
-            inline = bool(script.getInline())
-            if inline:
-                content = registry.getInlineResource(script.getId(), context)
-                data = {
-                    "inline": inline,
-                    "conditionalcomment": script.getConditionalcomment(),
-                    "content": content,
-                }
+            if script.isExternalResource():
+                src = "%s" % (script.getId(),)
             else:
-                if script.isExternalResource():
-                    src = "%s" % (script.getId(),)
-                else:
-                    src = "%s/%s/%s" % (registry_url, skinname, script.getId())
-                data = {
-                    "inline": inline,
-                    "conditionalcomment": script.getConditionalcomment(),
-                    "src": src,
-                }
+                src = "%s/%s/%s" % (registry_url, skinname, script.getId())
+            data = {
+                "src": src,
+            }
             result.append(data)
         return result
