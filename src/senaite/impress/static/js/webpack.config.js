9ffeeb5f206fc3eb944f3ace1bf7d36fc1fa673e
Ramon Bartl <rb@ridingbytes.com>
2019-01-31 17:56:29 +0100

    Removed email JS and created new production build

--- src/senaite/impress/static/js/webpack.config.js
+++ src/senaite/impress/static/js/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    publish: path.resolve(__dirname, './src/publish.coffee'),
+    publish: path.resolve(__dirname, './src/publish.coffee')
   },
   output: {
     filename: 'senaite.impress.[name].js',
