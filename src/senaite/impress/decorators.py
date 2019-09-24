8b4624cea3209de79cfc37693861ddc1879cda12
Ramon Bartl <rb@ridingbytes.com>
2019-09-24 13:39:40 +0200

    Handle `None` values in decorator more gracefully

--- src/senaite/impress/decorators.py
+++ src/senaite/impress/decorators.py
@@ -78,8 +78,7 @@ def returns_super_model(func):
 
         # Only portal objects are supported
         if not api.is_object(obj):
-            raise TypeError("Expected a portal object, got '{}'"
-                            .format(type(obj)))
+            return None
 
         # Wrap the object into a specific Publication Object Adapter
         uid = api.get_uid(obj)
@@ -93,7 +92,7 @@ def returns_super_model(func):
     def decorator(*args, **kwargs):
         obj = func(*args, **kwargs)
         if isinstance(obj, (list, tuple)):
-            return map(to_super_model, obj)
+            return filter(None, map(to_super_model, obj))
         return to_super_model(obj)
 
     return decorator
