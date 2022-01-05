45340660f9fb7422ca014949113a4030ed878c42
Ramon Bartl <rb@ridingbytes.com>
2022-01-05 12:57:18 +0100

    Added upgrade step

--- src/senaite/impress/upgrades/handlers.py
+++ src/senaite/impress/upgrades/handlers.py
@@ -23,6 +23,17 @@ from senaite.impress import logger
 PROFILE_ID = "profile-senaite.impress:default"
 
 
+def to_2000(portal_setup):
+    """Migration from version 1200 -> 2000
+
+    :param portal_setup: The portal_setup tool
+    """
+
+    logger.info("Run all import steps from SENAITE IMPRESS ...")
+    portal_setup.runAllImportStepsFromProfile(PROFILE_ID)
+    logger.info("Run all import steps from SENAITE IMPRESS [DONE]")
+
+
 def to_1200(portal_setup):
     """Migration from version 1000 -> 1200
 
