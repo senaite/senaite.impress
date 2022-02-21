657b5da557efd4b765274f41cf4bdbda521e15c5
Ramon Bartl <rb@ridingbytes.com>
2022-02-21 23:02:30 +0100

    Implemented report wrapper adapter

--- src/senaite/impress/interfaces.py
+++ src/senaite/impress/interfaces.py
@@ -19,6 +19,7 @@
 # Some rights reserved, see README and LICENSE.
 
 from bika.lims.interfaces import IBikaLIMS
+from zope.interface import Attribute
 from zope.interface import Interface
 from zope.viewlet.interfaces import IViewletManager
 
@@ -71,3 +72,11 @@ class IPdfReportStorage(Interface):
         """Stores the generated PDF for the given UIDs in a location and
         returns a list of generated report objects.
         """
+
+
+class IReportWrapper(Interface):
+    """Wrapper class for reports
+    """
+    pdf = Attribute("PDF data")
+    png = Attribute("PNG image data")
+    metadata = Attribute("Report metadata")
---
+++ src/senaite/impress/reportwrapper.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from bika.lims import api
+from DateTime import DateTime
+from senaite.impress.interfaces import IReportWrapper
+from zope.interface import implementer
+
+
+@implementer(IReportWrapper)
+class ReportWrapper(object):
+    """Report wrapper to generate PDF/PNG
+    """
+    def __init__(self,
+                 html,
+                 collection,
+                 template,
+                 paperformat,
+                 orientation,
+                 report_options,
+                 publisher):
+
+        self._pdf = None
+        self.html = html
+        self.collection = collection
+        self.template = template
+        self.paperformat = paperformat
+        self.orientation = orientation
+        self.report_options = report_options
+        self.publisher = publisher
+        self.created = DateTime()
+
+    @property
+    def pdf(self):
+        """Returns or generates the PDF on the fly
+        """
+        if self._pdf is None:
+            self._pdf = self.publisher.write_pdf(self.html)
+        return self._pdf
+
+    @property
+    def metadata(self):
+        """Returns the report metadata as a dictionary
+        """
+        return {
+            "template": self.template,
+            "paperformat": self.paperformat,
+            "orientation": self.orientation,
+            "timestamp": self.created.ISO(),
+        }
+
+    def __repr__(self):
+        return "<ReportWrapper for %s>" % ",".join(map(api.get_id, self.collection))
