d89c77eb008779a40027eadb8cd97e8134c6ad5a
Jordi Puiggené <jp@naralabs.com>
2022-05-20 13:12:38 +0200

    Only one results report is generated when SingleReport template is selected (#123)
    * Fix only the first sample is displayed in report preview
    
    * Group the exit urls by path
    
    * Remove pdb
    
    * Safe exit_url

--- src/senaite/impress/ajax.py
+++ src/senaite/impress/ajax.py
@@ -23,6 +23,7 @@ import json
 
 from bika.lims import _
 from bika.lims import api
+from collections import OrderedDict
 from DateTime import DateTime
 from senaite.app.supermodel import SuperModel
 from senaite.impress import logger
@@ -274,7 +275,16 @@ class AjaxPublishView(PublishView):
         if not exit_urls:
             return api.get_url(self.context)
 
-        return exit_urls[0]
+        # Group the urls by path. This makes possible to at least return a
+        # single url for multiple uids when the base path (e.g. client) is the
+        # same. This is required for Single Reports, for which there are as many
+        # report groups as samples, regardless of clients
+        groups = OrderedDict()
+        for url in exit_urls:
+            base_path, uids = url.split("?uids=")
+            path_uids = groups.get(base_path, "")
+            groups[base_path] = ",".join(filter(None, [path_uids, uids]))
+        return "?uids=".join(groups.items()[0])
 
     def get_exit_url_for(self, reports, action="save"):
         """Handle the response for the generated reports
@@ -309,9 +319,10 @@ class AjaxPublishView(PublishView):
         # generate exit URL
         exit_url = self.context.absolute_url()
         if clients:
-            exit_url = "{}/{}?uids={}".format(
-                api.get_url(clients[0]), endpoint, ",".join(report_uids))
+            exit_url = api.get_url(clients[0])
 
+        exit_url = "{}/{}?uids={}".format(exit_url, endpoint,
+                                          ",".join(report_uids))
         return exit_url
 
     def ajax_get_reports(self, *args):
