91ea8e6439d161e5b5ab060352d295b452f2c4bd
Jordi Puiggené <jp@naralabs.com>
2022-05-20 12:47:22 +0200

    Remove pdb

--- src/senaite/impress/ajax.py
+++ src/senaite/impress/ajax.py
@@ -279,7 +279,6 @@ class AjaxPublishView(PublishView):
         # single url for multiple uids when the base path (e.g. client) is the
         # same. This is required for Single Reports, for which there are as many
         # report groups as samples, regardless of clients
-        import pdb;pdb.set_trace()
         groups = OrderedDict()
         for url in exit_urls:
             base_path, uids = url.split("?uids=")
