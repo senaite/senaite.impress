c2f813e13e9d94c0257997ec74632d12c935c291
Ramon Bartl <rb@ridingbytes.com>
2022-10-03 11:29:27 +0200

    Explicit upgrade handler

--- src/senaite/impress/upgrades/handlers.py
+++ src/senaite/impress/upgrades/handlers.py
@@ -23,8 +23,8 @@ from senaite.impress import logger
 PROFILE_ID = "profile-senaite.impress:default"
 
 
-def run_all_importsteps(portal_setup):
-    """Run all import steps
+def to_2300(portal_setup):
+    """Update to version 2.3.0
 
     :param portal_setup: The portal_setup tool
     """
