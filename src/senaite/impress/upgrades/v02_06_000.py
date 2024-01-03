78175140b901a551b3af714dd78ddfaa07d0f6da
Ramon Bartl <rb@ridingbytes.com>
2024-01-03 11:06:58 +0100

    Added upgrade steps for 2.6.0

---
+++ src/senaite/impress/upgrades/v02_06_000.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.IMPRESS.
+#
+# SENAITE.IMPRESS is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2018-2024 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+from senaite.impress import logger
+
+PROFILE_ID = "profile-senaite.impress:default"
+
+
+def upgrade(portal_setup):
+    """Update to version 2.6.0
+
+    :param portal_setup: The portal_setup tool
+    """
+    logger.info("Run all import steps from SENAITE IMPRESS ...")
+    portal_setup.runAllImportStepsFromProfile(PROFILE_ID)
+    logger.info("Run all import steps from SENAITE IMPRESS [DONE]")
